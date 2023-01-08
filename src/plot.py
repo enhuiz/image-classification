837b3e4291a7deb0e744da56cfdcfe9f17835fac
enhuiz <niuzhe.nz@outlook.com>
2023-01-08 16:35:35 +0800

    Also plot testing

--- src/plot.py
+++ src/plot.py
@@ -7,6 +7,54 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
+def plot(paths, args):
+    dfs = []
+
+    for path in paths:
+        with open(path, "r") as f:
+            text = f.read()
+
+        rows = []
+
+        pattern = r"(\{.+?\})"
+
+        for row in re.findall(pattern, text, re.DOTALL):
+            try:
+                row = json.loads(row)
+            except Exception as e:
+                continue
+
+            if "global_step" in row:
+                rows.append(row)
+
+        df = pd.DataFrame(rows)
+
+        if "name" in df:
+            df["name"] = df["name"].fillna("train")
+        else:
+            df["name"] = "train"
+
+        df["group"] = str(path.parents[args.group_level])
+        df["group"] = df["group"] + "/" + df["name"]
+
+        dfs.append(df)
+
+    df = pd.concat(dfs)
+
+    for group_name, gdf in df.groupby("group"):
+        for y in args.ys:
+            gdf = gdf.sort_values("global_step")
+            print(gdf)
+
+            gdf.plot(
+                x="global_step",
+                y=y,
+                label=f"{group_name}/{y}",
+                ax=plt.gca(),
+                marker="x" if len(gdf) < 100 else None,
+            )
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("ys", nargs="+")
@@ -18,32 +66,7 @@ def main():
     args = parser.parse_args()
 
     paths = args.log_dir.rglob(f"**/{args.filename}")
-
-    dfs = []
-
-    for path in paths:
-        with open(path, "r") as f:
-            text = f.read()
-
-        rows = []
-
-        pattern = r"(\{.+?\})"
-        for record in re.findall(pattern, text, re.DOTALL):
-            if "name" not in record and ("global_step" in record):
-                rows.append(json.loads(record))
-
-        df = pd.DataFrame(rows)
-        df["group"] = path.parents[args.group_level]
-
-        dfs.append(df)
-
-    df = pd.concat(dfs)
-
-    for group_name, gdf in df.groupby("group"):
-        for y in args.ys:
-            gdf = gdf.sort_values("global_step")
-            gdf.plot(x="global_step", y=y, label=f"{group_name}/{y}", ax=plt.gca())
-
+    plot(paths, args)
     plt.savefig(args.out_path)
 
 
