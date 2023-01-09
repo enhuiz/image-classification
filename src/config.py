1f2ece510c454a141d4989ecf256cccde099e0be
enhuiz <niuzhe.nz@outlook.com>
2023-01-09 16:49:06 +0800

    Constant lr

--- src/config.py
+++ src/config.py
@@ -26,6 +26,7 @@ class Config(ConfigBase):
     normalizer: bool = True
     per_channel_normalizer: bool = False
     tailor_num_classes: bool = False
+    constant_lr: float | None = None
 
     @property
     def num_classes(self):
@@ -37,21 +38,44 @@ class Config(ConfigBase):
             raise NotImplementedError(self.dataset)
 
     @property
+    def warmup_decay_scheduler(self):
+        return {
+            "type": "WarmupDecayLR",
+            "params": {
+                "warmup_min_lr": self.warmup_min_lr,
+                "warmup_max_lr": self.warmup_max_lr,
+                "warmup_num_steps": self.warmup_num_steps,
+                "total_num_steps": self.max_iter,
+                "warmup_type": "linear",
+            },
+        }
+
+    @property
+    def constant_scheduler(self):
+        return {
+            "type": "WarmupLR",
+            "params": {
+                # Keep the same
+                "warmup_min_lr": self.constant_lr,
+                "warmup_max_lr": self.constant_lr,
+                "warmup_num_steps": 1,
+            },
+        }
+
+    @property
+    def scheduler(self):
+        if self.constant_lr is not None:
+            return self.constant_scheduler
+        else:
+            return self.warmup_decay_scheduler
+
+    @property
     def ds_cfg(self):
         return {
             "train_micro_batch_size_per_gpu": self.batch_size,
             "gradient_accumulation_steps": 1,
             "optimizer": {"type": "Adam"},
-            "scheduler": {
-                "type": "WarmupDecayLR",
-                "params": {
-                    "warmup_min_lr": self.warmup_min_lr,
-                    "warmup_max_lr": self.warmup_max_lr,
-                    "warmup_num_steps": self.warmup_num_steps,
-                    "total_num_steps": self.max_iter,
-                    "warmup_type": "linear",
-                },
-            },
+            "scheduler": self.scheduler,
             "gradient_clipping": self.gradient_clipping,
         }
 
